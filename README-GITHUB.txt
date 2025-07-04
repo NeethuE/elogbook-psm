1637e06c34470ab7cd8678b6907f35db4d338643
NeethuE <jassjin266@gmail.com>
2025-07-04 10:24:36 +0530

    Add files via upload

--- README-GITHUB.txt
+++ README-GITHUB.txt
@@ -1,12 +1,3 @@
-# E-Logbook for Department of PSM, RUHS
+# Fixed E-Logbook
 
-## 🚀 Deployment Instructions (Without Firebase CLI)
-
-1. Create a GitHub repository at https://github.com
-2. Upload all files from this folder to the repo
-3. Go to https://console.firebase.google.com > log-book-13015 > Hosting
-4. Click **Get Started** > **Connect to GitHub**
-5. Authorize GitHub and select your repo
-6. Firebase will automatically deploy the site
-
-✅ You’ll get a public link like https://log-book-13015.web.app
+Contains corrected HTML files with Firebase Firestore and Storage integration.
--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js';
+
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "log-book-13015.firebaseapp.com",
   projectId: "log-book-13015",
+  storageBucket: "log-book-13015.appspot.com",
 };
-firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
